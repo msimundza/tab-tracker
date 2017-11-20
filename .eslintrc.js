7180f63c625a35a65e0932c2ffed41cdbfbecbd2
Marko Šimundža <markosimundza@gmail.com>
2017-11-20 03:44:20 +0100

    Vuejs client basic setup

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 2017
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-console": ["error", {
+            allow: ["log", "error"]
+        }]
+    }
+};
